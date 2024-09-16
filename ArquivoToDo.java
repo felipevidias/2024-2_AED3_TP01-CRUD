398fd683e506edd7c1c1afe191d45f44a3694f72
Felipe Vilhena Dias <121449886+felipevidias@users.noreply.github.com>
2024-09-16 14:11:59 -0300

    Update and rename ArquivoFilme.java to ArquivoToDo.java

--- ArquivoToDo.java
+++ ArquivoToDo.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Constructor;
 
 public class ArquivoToDo extends Arquivo<ToDo> {
 
-    public ArquivoFilme(Constructor<ToDo> construtor, String name) throws Exception {
+    public ArquivoToDo(Constructor<ToDo> construtor, String name) throws Exception {
         super(construtor, name);
     }
 }
